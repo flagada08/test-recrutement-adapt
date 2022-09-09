16ebcc036eae4353c8f6259c49e7338f189eb50e
TERENCE <flagada08@gmail.com>
2022-09-09 11:53:34 +0200

    responsive firstPart complete

--- src/App/script.js
+++ src/App/script.js
@@ -3,6 +3,8 @@
 let hamMenuIcon = document.getElementById("ham-menu");
 let navBar = document.getElementById("nav-bar");
 let navLinks = navBar.querySelectorAll("li");
+let btnSearch = document.getElementsByClassName("btn-search");
+let plsSearch = document.getElementsByClassName("plus-search");
 // listener pour l'évènement "click" sur l'élément icone du menu burger
 hamMenuIcon.addEventListener("click", () => {
   navBar.classList.toggle("active");
@@ -14,4 +16,14 @@ navLinks.forEach((navLinks) => {
     navBar.classList.remove("active");
     hamMenuIcon.classList.toggle("fa-times");
   });
+});
+// listener pour l'évènement "click" sur l'élément boutton du champ de
+// recherche
+btnSearch[0].addEventListener("click", () => {
+  console.log("clickmeonemoretime");
+});
+// listener pour l'évènement "click" sur l'élément boutton du champ de
+// recherche
+plsSearch[0].addEventListener("click", () => {
+  console.log("oupsididitagain");
 });
